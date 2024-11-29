29577b199fd0c9f223a749c68f74cf9a9b1c0ac2
Damian Socha <damian.socha@amigo.ie>
2024-11-29 14:41:04 +0000

    returning number of applications owned by agency with default assigment

--- app/agents_default_assignments/serializers.py
+++ app/agents_default_assignments/serializers.py
@@ -1,17 +1,35 @@
+from django.db.models import Q
 from rest_framework import serializers
 
-from core.models import Assignment, User
+from core.models import Assignment, User, Application, Loan
 from user.serializers import TeamSerializer
 
 
 class SimpleUserSerializer(serializers.ModelSerializer):
     """Serializer to handle user ID and read-only email."""
     teams = TeamSerializer(many=True, read_only=True)  # Use the TeamSerializer
+    applications_owed_len = serializers.SerializerMethodField()
+    advancements_owed_len = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ['id', 'email', 'name', 'is_active', 'country', 'teams']
+        fields = ['id', 'email', 'name', 'is_active', 'country', 'teams', 'applications_owed_len',
+                  'advancements_owed_len']
         read_only_fields = ['email']  # Ensure email is read-only
+
+    def get_applications_owed_len(self, obj):
+        # Replace `user_field` with the actual field name that links the Application model to the User model.
+
+        return Application.objects.filter(user=obj, is_rejected=False, approved=False).count()
+
+    def get_advancements_owed_len(self, obj):
+        return Loan.objects.filter(
+            application__user=obj,
+            is_settled=False,
+            is_paid_out=False
+        ).filter(
+            Q(is_committee_approved=True) | Q(is_committee_approved__isnull=True)
+        ).count()
 
 
 class AssignmentSerializer(serializers.ModelSerializer):
